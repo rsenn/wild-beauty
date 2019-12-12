2552ac46980c4b2f40c1838968e454f52e820647
Roman Senn <roman.l.senn@gmail.com>
2019-12-12 17:12:54 +0100

    ...

---
+++ scripts/svgo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+for ARG; do 
+  svgo --indent=2 --pretty \
+    --enable={cleanupAttrs,cleanupEnableBackground,cleanupListOfValues,cleanupNumericValues,convertColors,convertPathData,convertShapeToPath,convertTransform,inlineStyles,mergePaths,moveGroupAttrsToElems,prefixIds,removeComments,removeDesc,removeDoctype,removeEditorsNSData,removeEmptyAttrs,removeEmptyContainers,removeEmptyText,removeHiddenElems,removeMetadata,removeNonInheritableGroupAttrs,removeOffCanvasPaths,removeRasterImages,removeScriptElement,removeTitle,removeUnknownsAndDefaults,removeUnusedNS,removeUselessDefs,removeUselessStrokeAndFill,removeXMLProcInst,reusePaths,sortAttrs} \
+    -i "$ARG" -o "${ARG##*/}"
+
+  sed -i 's,xlink:,,g' "${ARG##*/}" 
+done
---
+++ test-api.sh
@@ -0,0 +1,6 @@
+
+
+ curl -v --cookie-jar cookies.txt --header 'Content-Type: application/json' --data '{"username":"roman","password":"r4eHuJ"}'   http://localhost:5555/api/login
+
+ curl  -v --cookie cookies.txt http://localhost:5555/api/logout
+
