1d1a79a4308043a863bb93de89c1db81058f4eee
Roman Senn <roman.l.senn@gmail.com>
2020-01-04 19:50:38 +0100

    ...

--- scripts.old/remove-prettier-format-header.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -x
-exec sed -i '1 { /@format/ { N; /\n$/ { d } } }'  \
-  ${@:-$(find components utils stores pages -name "*.js")}
-
--- scripts.old/svgo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for ARG; do 
-  svgo --indent=2 --pretty \
-    --enable={cleanupAttrs,cleanupEnableBackground,cleanupListOfValues,cleanupNumericValues,convertColors,convertShapeToPath,convertTransform,inlineStyles,mergePaths,moveGroupAttrsToElems,prefixIds,removeComments,removeDesc,removeDoctype,removeEditorsNSData,removeEmptyAttrs,removeEmptyContainers,removeEmptyText,removeHiddenElems,removeMetadata,removeNonInheritableGroupAttrs,removeOffCanvasPaths,removeRasterImages,removeScriptElement,removeTitle,removeUnknownsAndDefaults,removeUnusedNS,removeUselessDefs,removeUselessStrokeAndFill,removeXMLProcInst,reusePaths,convertPathData,sortAttrs} \
-    -i "$ARG" -o "${ARG##*/}"
-
-  sed -i 's,xlink:,,g' "${ARG##*/}" 
-done
