ca739421ac85d2c6c818f25326d2b498930a3b80
Roman <roman.l.senn@gmail.com>
2020-01-18 19:12:07 +0100

    ...

--- components/views/grid.js
+++ components/views/grid.js
@@ -1,14 +1,38 @@
+import classNames from "classnames";
 import React from "react";
 import Util from "../../utils/util.js";
 
+export const Grid = ({ rows, cols, style, children, className, ...props }) => (
+  <React.Fragment>
+    <div
+      className={classNames(className, "grid-view")}
+      style={{
+        ...style,
+        display: "grid",
+        gridAutoRows: "20vh",
+        gridTemplateCols:
+          typeof rows == "number"
+            ? Util.repeat(cols, "" + 100 / cols + "%").join(" ")
+            : cols instanceof Array
+            ? cols.join(" ")
+            : cols
+      }}
+    >
+      {children}
+    </div>
+    <style jsx global>{`
+      .grid-view {
+        display: grid;
+        position: relative;
+      }
+    `}</style>
+  </React.Fragment>
+);
 
-export const Grid = ({ rows, cols }) => <React.Fragment>
-<div className={'grid-view'} style={{ gridTemplateRows: Util.repeat(rows, ' '+(100/ rows.length)+'%') }}  >
-</div>
-<style>{`
-.grid-view {
-  display: grid;
+export const GridItem = ({ col, row, style, children, className, ...props }) => (
+  <div style={{ ...style, gridRow: "" + row, gridCol: "" + col }} className={className || "grid-item"}>
+    {children}
+  </div>
+);
 
-}
-  `}</style>
-</React.Fragment>
+export default Grid;
--- pages/browse.js
+++ pages/browse.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { inject, observer } from "mobx-react";
 import axios from "../utils/axios.js";
-import { Grid } from "../components/views/grid.js";
+import { Grid, GridItem } from "../components/views/grid.js";
 import Util from "../utils/util.js";
 
 @inject("rootStore")
@@ -44,9 +44,45 @@ class Browse extends React.Component {
   }
 
   render() {
-    return <div>Browse
-
-    <Grid rows={5} cols={5} /></div>;
+    return (
+      <div className={'grid-container'}>
+        Browse
+        <Grid className={'browse-grid'} rows={`repeat(5, 20fr)`} cols={`repeat(5, 2fr)`} style={{ gridGap: '10px' }}>
+        <GridItem row={1} col={'1'} style={{ background: 'rgba(255,0,0,0.5)' }}>A</GridItem>
+        <GridItem row={1} col={'2'} style={{ background: 'rgba(255,0,0,0.5)' }}>B</GridItem>
+        <GridItem row={1} col={'3'} style={{ background: 'rgba(255,0,0,0.5)' }}>C</GridItem>
+        <GridItem row={1} col={'4'} style={{ background: 'rgba(255,0,0,0.5)' }}>D</GridItem>
+        <GridItem row={1} col={'5'} style={{ background: 'rgba(255,0,0,0.5)' }}>E</GridItem>
+        <GridItem row={3} col={'1'} style={{ background: 'rgba(0,127,0,0.5)' }}>A</GridItem>
+        <GridItem row={3} col={'2'} style={{ background: 'rgba(0,127,0,0.5)' }}>B</GridItem>
+        <GridItem row={5} col={'1'} style={{ background: 'rgba(0,60,255,0.5)' }}>C</GridItem>
+        <GridItem row={5} col={'2'} style={{ background: 'rgba(0,60,255,0.5)' }}>D</GridItem>
+        <GridItem row={5} col={'3'} style={{ background: 'rgba(0,60,255,0.5)' }}>E</GridItem>
+        </Grid>
+          <style jsx global>{`
+            .grid-container {
+display:flex;
+justify-content: center;
+            }
+            .browse-grid {
+margin: 50px 0 50px 0;
+border: 1px dashed black;
+    height: 100%;
+              width: 90vw;
+            }
+           .grid-container, .browse-grid {
+              overflow: visible;
+            }
+      .browse-grid > div, .grid-item {
+        background-color: rgba(255,0,0,0.5);
+ 
+      }
+      .grid-item {
+        box-shadow: 0px 0px 8px 1px rgba(0,0,0,1);
+      }
+  `}</style>
+      </div>
+    );
   }
 }
 
