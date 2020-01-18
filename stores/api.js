dac88eef32882d425fef70227fb184093ef4891d
Roman <roman.l.senn@gmail.com>
2020-01-18 22:04:32 +0100

    ...

--- stores/api.js
+++ stores/api.js
@@ -19,43 +19,29 @@ function API(url = "http://wild-beauty.herokuapp.com/v1/graphql") {
     if(res && res.text !== undefined) res = await res.text();
     if(typeof res == "string") res = JSON.parse(res);
     while((await res.data) !== undefined) res = await res.data;
-
     //console.log("res: ", res);
-    console.error("query: ", query);
-
+    //console.error("query: ", query);
     if(res.errors !== undefined) {
       console.error("res.errors: ", res.errors);
       throw Error(res.errors[0].message);
     }
-
     return res;
   };
 
   api.list = async function(name, fields = [], opts = {}) {
     const camelCase = Util.ucfirst(name);
     //if(typeof fields == "string") fields = fields.split(/[ ,;]/g);
-
     const { where, ...options } = opts;
     console.log("api.list ", { name, where, options });
     let objStr;
-
-    objStr = where
-      ? typeof where == "string"
-        ? "where: " + where
-        : "where: {" +
-          Object.entries(where)
-            .map(([key, value]) => `${key}: {` + (value === null ? `_is_null: true` : `_eq:"${value}"`) + `}`)
-            .join(", ") +
-          "}"
-      : "";
+    // prettier-ignore
+    objStr = where     ? typeof where == "string"? "where: " + where : "where: {" + Object.entries(where) .map(([key, value]) => `${key}: {` + (value === null ? `_is_null: true` : `_eq:"${value}"`) + `}`) .join(", ") + "}": "";
     objStr += Object.keys(options)
       .map(key => `${key}: ${options[key]}`)
       .join(", ");
     if(objStr.length) objStr = `(${objStr})`;
     const queryStr = `query List${camelCase} { ${name}${objStr} { ${fields} } }`;
-
     console.log("query: ", queryStr);
-
     let ret = await this(queryStr);
     if(typeof ret == "object" && ret[name] !== undefined) ret = ret[name];
     return ret;
@@ -63,16 +49,8 @@ function API(url = "http://wild-beauty.herokuapp.com/v1/graphql") {
 
   api.select = function(name, obj, fields = "") {
     const camelCase = Util.ucfirst(name);
-    const objStr =
-      "{ " +
-      (typeof obj == "string"
-        ? `where: ${obj} }`
-        : "where: { " +
-          Object.entries(obj)
-            .map(([key, value]) => `${key}: {_eq: ${value}}`)
-            .join(", ") +
-          "}") +
-      " }";
+    // prettier-ignore
+    const objStr =   "{ " + (typeof obj == "string"? `where: ${obj} }` : "where: { " + Object.entries(obj) .map(([key, value]) => `${key}: {_eq: ${value}}`) .join(", ") + "}") + " }";
     if(typeof fields == "string") fields = fields.split(/[ ,;]/g);
     const queryStr = `query Select${camelCase} { ${name}(${objStr}) { ${fields.join(" ")} } }`;
     console.log(`query: ${queryStr}`);
@@ -111,13 +89,8 @@ function API(url = "http://wild-beauty.herokuapp.com/v1/graphql") {
 
   api.insert = async function(name, obj, fields = []) {
     const camelCase = Util.ucfirst(name);
-    const objStr = Object.keys(obj)
-      .map(key =>
-        typeof obj[key] == "string" && key == "name" /* && !/^".*"$/.test(obj[key])*/
-          ? `${key}: "${obj[key]}"`
-          : `${key}: ${obj[key]}`
-      )
-      .join(", ");
+    // prettier-ignore
+    const objStr = Object.keys(obj) .map(key => typeof obj[key] == "string" && key == "name" /* && !/^".*"$/.test(obj[key])*/ ? `${key}: "${obj[key]}"` : `${key}: ${obj[key]}` ) .join(", ");
     const fieldStr = [/*...Object.keys(obj), */ ...fields].join(" ");
     const queryStr = `mutation Insert${camelCase} {
     __typename
