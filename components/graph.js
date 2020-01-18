cf6f1cfba5d0565089861667a15ff26366653ad2
Roman <roman.l.senn@gmail.com>
2020-01-19 00:46:17 +0100

    ...

--- components/graph.js
+++ components/graph.js
@@ -7,58 +7,68 @@ import windowSize from "react-window-size";
 import withSizes from "react-sizes";
 
 // prettier-ignore
+const collapsibleForceLayoutData = {  name: "flare", children: [{name: "animate", children: [{ name: "Easing", size: 17010 }, { name: "FunctionSequence", size: 5842 }, {name: "interpolate", children: [{ name: "ArrayInterpolator", size: 1983 }, { name: "ColorInterpolator", size: 2047 }, { name: "DateInterpolator", size: 1375 }, { name: "Interpolator", size: 8746 }, { name: "MatrixInterpolator", size: 2202 }, { name: "NumberInterpolator", size: 1382 }, { name: "ObjectInterpolator", size: 1629 }, { name: "PointInterpolator", size: 1675 }, { name: "RectangleInterpolator", size: 2042 } ] }, { name: "ISchedulable", size: 1041 }, { name: "Parallel", size: 5176 }, { name: "Pause", size: 449 }, { name: "Scheduler", size: 5593 }, { name: "Sequence", size: 5534 }, { name: "Transition", size: 9201 }, { name: "Transitioner", size: 19975 }, { name: "TransitionEvent", size: 1116 }, { name: "Tween", size: 6006 } ] }, {name: "physics", children: [{ name: "DragForce", size: 1082 }, { name: "GravityForce", size: 1336 }, { name: "IForce", size: 319 }, { name: "NBodyForce", size: 10498 }, { name: "Particle", size: 2822 }, { name: "Simulation", size: 9983 }, { name: "Spring", size: 2213 }, { name: "SpringForce", size: 1681 } ] }, {name: "query", children: [{ name: "AggregateExpression", size: 1616 }, { name: "And", size: 1027 }, { name: "Arithmetic", size: 3891 }, { name: "Average", size: 891 }, { name: "BinaryExpression", size: 2893 }, { name: "Comparison", size: 5103 }, { name: "CompositeExpression", size: 3677 }, { name: "Count", size: 781 }, { name: "DateUtil", size: 4141 }, { name: "Distinct", size: 933 }, { name: "Expression", size: 5130 }, { name: "ExpressionIterator", size: 3617 }, {name: "methods", children: [{ name: "add", size: 593 }, { name: "and", size: 330 }, { name: "average", size: 287 }, { name: "count", size: 277 }, { name: "distinct", size: 292 }, { name: "div", size: 595 }, { name: "eq", size: 594 }, { name: "fn", size: 460 }, { name: "gt", size: 603 }, { name: "gte", size: 625 }, { name: "iff", size: 748 }, { name: "isa", size: 461 }, { name: "lt", size: 597 }, { name: "lte", size: 619 }, { name: "max", size: 283 }, { name: "min", size: 283 }, { name: "mod", size: 591 }, { name: "mul", size: 603 }, { name: "neq", size: 599 }, { name: "not", size: 386 }, { name: "or", size: 323 }, { name: "orderby", size: 307 }, { name: "range", size: 772 }, { name: "select", size: 296 }, { name: "stddev", size: 363 }, { name: "sub", size: 600 }, { name: "sum", size: 280 }, { name: "update", size: 307 }, { name: "variance", size: 335 }, { name: "where", size: 299 }, { name: "xor", size: 354 }, { name: "_", size: 264 } ] }, { name: "Minimum", size: 843 }, { name: "Not", size: 1554 }, { name: "Or", size: 970 }, { name: "Query", size: 13896 }, { name: "Range", size: 1594 }, { name: "StringUtil", size: 4130 }, { name: "Sum", size: 791 }, { name: "Variable", size: 1124 }, { name: "Variance", size: 1876 }, { name: "Xor", size: 1101 } ] }, {name: "scale", children: [{ name: "IScaleMap", size: 2105 }, { name: "LinearScale", size: 1316 }, { name: "LogScale", size: 3151 }, { name: "OrdinalScale", size: 3770 }, { name: "QuantileScale", size: 2435 }, { name: "QuantitativeScale", size: 4839 }, { name: "RootScale", size: 1756 }, { name: "Scale", size: 4268 }, { name: "ScaleType", size: 1821 }, { name: "TimeScale", size: 5833 } ] }, {name: "vis", children: [{name: "axis", children: [{ name: "Axes", size: 1302 }, { name: "Axis", size: 24593 }, { name: "AxisGridLine", size: 652 }, { name: "AxisLabel", size: 636 }, { name: "CartesianAxes", size: 6703 } ] }, {name: "controls", children: [{ name: "AnchorControl", size: 2138 }, { name: "ClickControl", size: 3824 }, { name: "Control", size: 1353 }, { name: "ControlList", size: 4665 }, { name: "DragControl", size: 2649 }, { name: "ExpandControl", size: 2832 }, { name: "HoverControl", size: 4896 }, { name: "IControl", size: 763 }, { name: "PanZoomControl", size: 5222 }, { name: "SelectionControl", size: 7862 }, { name: "TooltipControl", size: 8435 } ] }, {name: "data", children: [{ name: "Data", size: 20544 }, { name: "DataList", size: 19788 }, { name: "DataSprite", size: 10349 }, { name: "EdgeSprite", size: 3301 }, { name: "NodeSprite", size: 19382 }, {name: "render", children: [{ name: "ArrowType", size: 698 }, { name: "EdgeRenderer", size: 5569 }, { name: "IRenderer", size: 353 }, { name: "ShapeRenderer", size: 2247 } ] }, { name: "ScaleBinding", size: 11275 }, { name: "Tree", size: 7147 }, { name: "TreeBuilder", size: 9930 } ] }, {name: "events", children: [{ name: "DataEvent", size: 2313 }, { name: "SelectionEvent", size: 1880 }, { name: "TooltipEvent", size: 1701 }, { name: "VisualizationEvent", size: 1117 } ] }, {name: "legend", children: [{ name: "Legend", size: 20859 }, { name: "LegendItem", size: 4614 }, { name: "LegendRange", size: 10530 } ] }, {name: "operator", children: [{name: "distortion", children: [{ name: "BifocalDistortion", size: 4461 }, { name: "Distortion", size: 6314 }, { name: "FisheyeDistortion", size: 3444 } ] }, {name: "encoder", children: [{ name: "ColorEncoder", size: 3179 }, { name: "Encoder", size: 4060 }, { name: "PropertyEncoder", size: 4138 }, { name: "ShapeEncoder", size: 1690 }, { name: "SizeEncoder", size: 1830 } ] }, {name: "filter", children: [{ name: "FisheyeTreeFilter", size: 5219 }, { name: "GraphDistanceFilter", size: 3165 }, { name: "VisibilityFilter", size: 3509 } ] }, { name: "IOperator", size: 1286 }, {name: "label", children: [{ name: "Labeler", size: 9956 }, { name: "RadialLabeler", size: 3899 }, { name: "StackedAreaLabeler", size: 3202 } ] }, {name: "layout", children: [{ name: "AxisLayout", size: 6725 }, { name: "BundledEdgeRouter", size: 3727 }, { name: "CircleLayout", size: 9317 }, { name: "CirclePackingLayout", size: 12003 }, { name: "DendrogramLayout", size: 4853 }, { name: "ForceDirectedLayout", size: 8411 }, { name: "IcicleTreeLayout", size: 4864 }, { name: "IndentedTreeLayout", size: 3174 }, { name: "Layout", size: 7881 }, { name: "NodeLinkTreeLayout", size: 12870 }, { name: "PieLayout", size: 2728 }, { name: "RadialTreeLayout", size: 12348 }, { name: "RandomLayout", size: 870 }, { name: "StackedAreaLayout", size: 9121 }, { name: "TreeMapLayout", size: 9191 } ] }, { name: "Operator", size: 2490 }, { name: "OperatorList", size: 5248 }, { name: "OperatorSequence", size: 4190 }, { name: "OperatorSwitch", size: 2581 }, { name: "SortOperator", size: 2023 } ] }, { name: "Visualization", size: 16540 } ] } ] };
+
+// prettier-ignore
 export const collapsibleForceLayout = data => {
   const width = 1000,
   height = 1000;
   let i = 0;
+  
+  if(!data)
+    data = collapsibleForceLayoutData;
+
   const root = d3.hierarchy(data);
   const transform = d3.zoomIdentity;
   let node, link;
-  
-  const svg = d3
-  .select("#d3-graph-svg")
-  .append("svg")
-  .call(
-    d3
-    .zoom()
-    .scaleExtent([1 / 2, 8])
-    .on("zoom", zoomed)
-    )
-  .append("g")
-  .attr("transform", "translate(40,0)");
-  
+
+  console.log("collapsibleForceLayout data=",data);
+
+  const svgElem = d3
+    .select("#d3-graph-svg");
+
+  let svg = svgElem
+    .append("g")
+    .call(d3 .zoom() .scaleExtent([1 / 2, 8]) .on("zoom", zoomed) )
+    .append("g")
+    .attr("transform", "translate(-45,10) scale(0.25,0.25)");
+
+
   const simulation = d3
-  .forceSimulation()
-  .force("link", d3.forceLink().id(function(d) {return d.id; }) )
-  .force("charge", d3 .forceManyBody() .strength(-15) .distanceMax(300) )
-  .force("center", d3.forceCenter(width / 2, height / 4))
-  .on("tick", ticked);
+    .forceSimulation()
+    .force("link", d3.forceLink().id(function(d) {return d.id; }) )
+    .force("charge", d3 .forceManyBody() .strength(-15) .distanceMax(300) )
+    .force("center", d3.forceCenter(width / 2, height / 4))
+    .on("tick", ticked);
 
   function update() {
     const nodes = flatten(root);
     const links = root.links();
-    link = svg.selectAll(".link").data(links, function(d) {
-      return d.target.id;
-    });
+
+console.log("collapsibleForceLayout update ", { nodes,links});
+
+    link = svg.selectAll(".link").data(links, function(d) {return d.target.id; });
+
     link.exit().remove();
+    
     const linkEnter = link
     .enter()
     .append("line")
     .attr("class", "link")
     .style("stroke", "#000")
-    .style("opacity", "0.2")
-    .style("stroke-width", 2);
+    .style("opacity", "1")
+    .style("stroke-width", 0.8);
+
     link = linkEnter.merge(link);
-    node = svg.selectAll(".node").data(nodes, function(d) {
-      return d.id;
-    });
+    node = svg.selectAll(".node").data(nodes, function(d) {return d.id; });
     node.exit().remove();
+
     const nodeEnter = node
     .enter()
     .append("g")
     .attr("class", "node")
-    .attr("stroke", "#666")
-    .attr("stroke-width", 2)
+    .attr("stroke", "#000")
+    .attr("stroke-width", 1)
     .style("fill", color)
     .style("opacity", 1)
     .on("click", clicked)
@@ -86,7 +96,9 @@ export const collapsibleForceLayout = data => {
     return num;
   }
   function color(d) {
-    return d._children ? "#51A1DC" // collapsed package : d.children ? "#51A1DC" // expanded package : "#F94B4C"; // leaf node
+    return !!d._children  ? "#40df20" // collapsed package 
+    : (!!d.children ? "#ffc000" // expanded package 
+    : "#8000ff"); // leaf node
   }
   function radius(d) {
     return d._children ? 8 : d.children ? 8 : 4;
@@ -154,7 +166,7 @@ function forceSimulation(data) {
   .force('link', d3.forceLink().id(function(d) { return d.id; }))
   .force('charge', d3.forceManyBody().strength(-15).distanceMax(300))
   .force('center', d3.forceCenter( 0,0   ))
-  .on('tick', ticked)
+  .on('tick', ticked);
   /*
   let simulation = d3
     .forceSimulation()
@@ -162,43 +174,45 @@ function forceSimulation(data) {
       .force("charge", d3.forceManyBody().strength(d => {return -Math.pow(d.group, 2.5); }))
     .force("center", d3.forceCenter(0, 0))
 //   .force('charge', d3.forceManyBody().strength(2))
-.alphaTarget(0.2)*/ const grp = svg
-.append("g")
-.attr("transform", `translate(100,100) scale(0.5,0.5)`);
+.alphaTarget(0.2)*/
 
-const link = grp
-.append("g")
-.attr("class", "links")
-.selectAll("line")
-.data(data.links)
-.enter()
-.append("line")
-.attr("stroke-width", "0.5")
-.attr("stroke", "#000")
-.attr("opacity", 0.5);
+  const grp = svg.append("g").attr("transform", `translate(100,100) scale(0.5,0.5)`);
 
-const getNodeColor = function(a = 1.0) {
-  return d => {
-    let hv = Util.randInt(-22, 22),
-    sv = Util.randInt(-22, 22),
-    lv = Util.randInt(-22, 22);
-    const hsla = new HSLA(((d.group * 360) / 9 + hv) % 360, 100, 50, a);
-    const rgba = hsla.toRGBA();
-    return rgba.hex();
+  const link = grp
+    .append("g")
+    .attr("class", "links")
+    .selectAll("line")
+    .data(data.links)
+    .enter()
+    .append("line")
+    .attr("class", "link")
+    .attr("stroke-width", "0.5")
+    .attr("stroke", "#000")
+    .attr("opacity", 0.5);
+
+  const getNodeColor = function(a = 1.0) {
+    return d => {
+      let hv = Util.randInt(-22, 22),
+        sv = Util.randInt(-22, 22),
+        lv = Util.randInt(-22, 22);
+      const hsla = new HSLA(((d.group * 360) / 9 + hv) % 360, 100, 50, a);
+      const rgba = hsla.toRGBA();
+      return rgba.hex();
+    };
   };
-};
 
-const node = grp
-.append("g")
-.attr("class", "nodes")
-.attr("stroke", "#000")
-.attr("stroke-width", "0.3")
-.selectAll("circle")
-.data(data.nodes)
-.enter()
-.append("circle")
-.attr("r", d => Math.log(d.group * 4) * 2)
-.attr("fill", getNodeColor(1));
+  const node = grp
+    .append("g")
+    .attr("class", "nodes")
+    .attr("stroke", "#000")
+    .attr("stroke-width", "0.3")
+    .selectAll("circle")
+    .data(data.nodes)
+    .enter()
+    .append("circle")
+    .attr("class", "node")
+    .attr("r", d => Math.log(d.group * 4) * 2)
+    .attr("fill", getNodeColor(1));
 
   // prettier-ignore
   node.append("title").text(d => {return d.id; });
@@ -229,38 +243,36 @@ export class Graph extends Component {
     /* this.svgRef.subscribe(function(value) {
    console.log("value:", value);
  });*/
-}
+  }
 
-componentDidUpdate() {
-  console.log("Graph.componentDidUpdate");
-}
+  componentDidUpdate() {
+    console.log("Graph.componentDidUpdate");
+  }
 
-componentDidMount() {
-  let elem = this.svgRef.current;
-  console.log("Graph.componentDidMount svgRef", this.svgRef);
-  elem = Element.find("#d3-graph-svg");
-  if(!elem) return;
+  componentDidMount() {
+    let elem = this.svgRef.current;
+    console.log("Graph.componentDidMount svgRef", this.svgRef);
+    elem = Element.find("#d3-graph-svg");
+    if(!elem) return;
+    const { data } = this.props;
+    console.log("data: ", data);
+    collapsibleForceLayout();
+//     forceSimulation(data);
+  }
 
-  const { data } = this.props;
-
-  console.log("data: ", data);
-
-  forceSimulation(data);
-}
-
-render() {
-  return (
-    <div style={{ position: "absolute", top: 0 }}>
-    <svg
-    id="d3-graph-svg"
-    viewBox={`0 0 200 200`}
-    width={200}
-    height={200}
-    style={{ width: "100vw", height: "auto" }}
-    />
-    </div>
+  render() {
+    return (
+      <div style={{ position: "absolute", top: 0 }}>
+        <svg
+          id="d3-graph-svg"
+          viewBox={`0 0 200 200`}
+          width={200}
+          height={200}
+          style={{ width: "100vw", height: "auto" }}
+        />
+      </div>
     );
-}
+  }
 }
 
 const mapSizesToProps = ({ width, height }) => ({
