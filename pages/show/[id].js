8c0ea13cd84ca41cfc24cab58bc9267b9fa60fca
Roman <roman.l.senn@gmail.com>
2020-01-18 13:53:26 +0100

    ...

--- pages/show/[id].js
+++ pages/show/[id].js
@@ -4,5 +4,8 @@ const { initialMobxState, router, className } = props;
 const { query } = router;
 
 console.log(`Post: req:`, query);
- res.end(`Post: req:`, query.id);
+// res.end(`Post: req:`, query.id);
+
+  return <div><h4>Id: {query.id}</h4>
+  </div>
 };
