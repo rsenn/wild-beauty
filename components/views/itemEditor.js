95624e4536f36c6f4b6b6ce8958c4853cd85f031
Roman Senn <roman.l.senn@gmail.com>
2021-09-22 00:31:34 +0200

    ...

--- components/views/itemEditor.js
+++ components/views/itemEditor.js
@@ -11,7 +11,7 @@ import { makeItemToOption, findInTree } from "../../stores/functions.js";
 //import SortableTree from "react-sortable-tree";
 
 import DropdownTreeSelect from "react-dropdown-tree-select";
-import "../..static/css/react-dropdown-tree-select.css";
+import "../../static/css/react-dropdown-tree-select.css";
 
 //import "react-sortable-tree/style.css"; // once app
 
